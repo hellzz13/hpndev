796cd9ee3ca1f42d11f49d5b88e9506a6dd9482d
unknown <hpalenca13@gmail.com>
2024-10-31 07:51:20 -0300

    chore remove botão mobile

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -47,17 +47,17 @@ export default function Navbar() {
         <>
           <div className="max-w-7xl ml-2 md:mx-auto px-4 sm:px-6 lg:px-8">
             <div className="relative flex h-16 items-center justify-between">
-              <div className="absolute inset-y-0 right-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+              {/* <div className="absolute inset-y-0 right-0 flex items-center sm:hidden"> */}
+              {/* Mobile menu button*/}
+              {/* <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
                     <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
                   ) : (
                     <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
                   )}
-                </Disclosure.Button>
-              </div>
+                </Disclosure.Button> */}
+              {/* </div> */}
               <div className="flex items-center text-3xl font-black text-gray-800 dark:text-white">
                 <svg
                   width="25"
